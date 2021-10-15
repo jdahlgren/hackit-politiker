20f1ee195336806108b2a9be4a1c3f8bd37524ec
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2021-10-15 09:56:21 +0200

    init

---
+++ src/main/java/se/knowit/hackit/politiker/HackitPolitikerApplication.java
@@ -0,0 +1,13 @@
+package se.knowit.hackit.politiker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackitPolitikerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HackitPolitikerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/knowit/hackit/politiker/HackitPolitikerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.knowit.hackit.politiker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackitPolitikerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
